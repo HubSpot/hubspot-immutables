0fa522369ab7604e6bc1f90ecfff3daf43a687a0
jhaber <jhaber@hubspot.com>
2019-11-02 23:39:21 -0400

    Clarify

--- hubspot-style/src/main/java/com/hubspot/immutables/utils/WireSafeEnum.java
+++ hubspot-style/src/main/java/com/hubspot/immutables/utils/WireSafeEnum.java
@@ -127,7 +127,7 @@ public final class WireSafeEnum<T extends Enum<T>> {
     mapping is not bijective. For example, two enum constants might
     both be aliased to the same JSON value. When we encounter such
     a JSON value, we'll defer to the enum's deserialization behavior.
-    This should match the behavior if the enum wasn't wrapped in
+    This should match the behavior as if the enum wasn't wrapped in
     WireSafeEnum.
      */
     T[] deserializedConstants = MAPPER.convertValue(
