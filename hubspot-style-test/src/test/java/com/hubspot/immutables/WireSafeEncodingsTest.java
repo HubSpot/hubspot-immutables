70d310389227f364c55192a1ed1369e0a603ab4a
Peter Teixeira <pteixeira@hubspot.com>
2025-06-17 08:51:47 -0400

    JUnit 4 -> JUnit 5

--- hubspot-style-test/src/test/java/com/hubspot/immutables/WireSafeEncodingsTest.java
+++ hubspot-style-test/src/test/java/com/hubspot/immutables/WireSafeEncodingsTest.java
@@ -3,7 +3,7 @@ package com.hubspot.immutables;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.hubspot.immutables.utils.WireSafeEnum;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class WireSafeEncodingsTest {
 
